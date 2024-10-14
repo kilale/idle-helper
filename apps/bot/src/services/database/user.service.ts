b455b9b324cfdfb6ffb5784e40983a15f8355d72
kilale <himbrr@hotmail.de>
2024-10-14 17:28:51 +0200

    fixed bug in claim

--- apps/bot/src/services/database/user.service.ts
+++ apps/bot/src/services/database/user.service.ts
@@ -88,7 +88,8 @@ const claimFarm = async ({userId}: IClaimFarm): Promise<IUser | null> => {
       $set: {
         'farms.lastClaimedAt': new Date(),
         'farms.itemsUsed.timeCompressor': 0,
-        'farms.itemsUsed.timeSpeeder': 0
+        'farms.itemsUsed.timeSpeeder': 0,
+        'farms.itemsUsed.timeDilator': 0
       }
     },
     {
