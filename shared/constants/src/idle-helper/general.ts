5c9f6048b9be3796f3d1499b5ea3a78439426505
kilale <himbrr@hotmail.de>
2024-10-14 16:27:24 +0200

    updated links

--- shared/constants/src/idle-helper/general.ts
+++ shared/constants/src/idle-helper/general.ts
@@ -15,6 +15,6 @@ export const USER_ACC_OFF_ACTIONS = {
   skip: 'skip'
 } as const;
 
-export const SUPPORT_SERVER_INVITE_LINK = 'https://discord.gg/magXasew7C';
+export const SUPPORT_SERVER_INVITE_LINK = 'https://discord.gg/GYcFwdhMgd';
 export const BOT_INVITE_LINK =
-  'https://discord.com/api/oauth2/authorize?client_id=1133034852611018783&permissions=274878254144&scope=bot%20applications.commands';
+  '';
