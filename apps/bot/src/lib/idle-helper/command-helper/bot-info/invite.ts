1ec23fa9ba4a1d97959b353f611b1eaca335937a
kilale <himbrr@hotmail.de>
2024-10-14 21:49:12 +0200

    again formatting

--- apps/bot/src/lib/idle-helper/command-helper/bot-info/invite.ts
+++ apps/bot/src/lib/idle-helper/command-helper/bot-info/invite.ts
@@ -8,23 +8,10 @@ import {
 export const _invite = async (): Promise<EmbedBuilder> => {
   const embed = new EmbedBuilder().setColor(BOT_COLOR.embed);
   embed.setTitle('Cult Helper Invite');
-
-/*   embed.addFields(
-    {
-      name: '** **',
-      value: 'Cult Helper is __invite only__ bot, if you want to get access:\n\n- join the support server\n- contribute to the project',
-      inline: false
-    },
-    {
-      name: '** **',
-      value: `** **\n**Join the Cult Helper support server:**\n**[Support Server](${SUPPORT_SERVER_INVITE_LINK})**`,
-      inline: false
-    }
-  ); */
   embed.addFields(
     {
-      name: 'Cult Helper is __invite only__ bot, if you want to get access:',
-      value: '- join the support server\n- contribute to the project'
+      name: '',
+      value: 'Cult Helper is __invite only__ bot, if you want to get access:\n- join the support server\n- contribute to the project'
     },
     {
       name: 'Join the Cult Helper support server:',
