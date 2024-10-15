47043c8982aa2ce2a34746ba749ffb0dec82ef31
kilale <himbrr@hotmail.de>
2024-10-15 17:34:06 +0200

    added some logging

--- apps/bot/src/lib/idle-farm/message-readers/worker-assign.ts
+++ apps/bot/src/lib/idle-farm/message-readers/worker-assign.ts
@@ -7,8 +7,11 @@ interface IWorkerAssignReader {
 
 export const _workerAssignReader = ({content}: IWorkerAssignReader) => {
   const farmId = content.match(/farm `(\d+)`/)?.[1] ?? '0';
+  console.log('Recognized farmId: ' + farmId);
   const workerType = content.match(/`(.*) worker` successfully/)?.[1];
+  console.log('Recognized workerType: ' + workerType);
   const replacedWorker = content.match(/replaced <.+> `(.*) worker`/)?.[1];
+  console.log('Recognized replacedWorker: ' + replacedWorker);
 
   return {
     farmId,
